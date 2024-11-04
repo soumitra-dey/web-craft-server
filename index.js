933fa6005ba6c26878f994a0bd57fdd537d94879
soumitra-quest <soumitra.d@questlabs.biz>
2024-11-04 11:23:07 +0530

    fix cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
